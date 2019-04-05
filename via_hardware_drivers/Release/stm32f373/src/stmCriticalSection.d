da05429fdacb7f7c406bcd1d3cbf1a4164e9ef0d
Will <liquidcitymotors@gmail.com>
2019-04-05 03:42:52 -0500

    update gitignore and cleanup

--- via_hardware_drivers/Release/stm32f373/src/stmCriticalSection.d
+++
@@ -1,4 +0,0 @@
-stm32f373/src/stmCriticalSection.o: ../stm32f373/src/stmCriticalSection.c \
- /Users/willmitchell/Documents/via_hardware_executables/via_hardware_drivers/stm32f373/inc/stmCriticalSection.h
-
-/Users/willmitchell/Documents/via_hardware_executables/via_hardware_drivers/stm32f373/inc/stmCriticalSection.h:
