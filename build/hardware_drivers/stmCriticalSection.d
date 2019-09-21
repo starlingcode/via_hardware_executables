f082ae0c3094e959fefda6d639403e5cb469e2cb
liquidcitymotors <rileylakemusic@gmail.com>
2019-09-21 02:58:05 +0000

    first headless build attempt

---
+++ build/hardware_drivers/stmCriticalSection.d
@@ -0,0 +1,5 @@
+build/hardware_drivers/stmCriticalSection.o: \
+ hardware_drivers/stm32f373/src/stmCriticalSection.c \
+ hardware_drivers/stm32f373/inc/stmCriticalSection.h
+
+hardware_drivers/stm32f373/inc/stmCriticalSection.h:
