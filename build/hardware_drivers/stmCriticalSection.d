78c94a965e745fed3ae468d84d00cbeef76688e0
liquidcitymotors <rileylakemusic@gmail.com>
2019-09-21 03:14:11 +0000

    remove build artifacts

--- build/hardware_drivers/stmCriticalSection.d
+++
@@ -1,5 +0,0 @@
-build/hardware_drivers/stmCriticalSection.o: \
- hardware_drivers/stm32f373/src/stmCriticalSection.c \
- hardware_drivers/stm32f373/inc/stmCriticalSection.h
-
-hardware_drivers/stm32f373/inc/stmCriticalSection.h:
